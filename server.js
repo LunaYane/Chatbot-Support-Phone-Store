773a059d34455926253d3da35e6199bf782a601a
Nguyen Thanh Hai <thanhhai141002@gmail.com>
2026-04-02 14:42:15 +0700

    Change default server port to 3001

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const phones = require('./data/phones');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
